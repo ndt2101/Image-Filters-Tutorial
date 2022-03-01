48b74c8fcf7b2b0229f904f702e75ef6e8a80222
ndt2101 <69622101+ndt2101@users.noreply.github.com>
2022-03-01 18:39:37 +0700

    first commit

---
+++ app/src/main/java/com/tuan2101/imagefilters/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tuan2101.imagefilters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
